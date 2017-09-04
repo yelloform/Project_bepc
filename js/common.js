a574c7280bd351d8f0418d569ea0d78840c11859
Lightene <Lightene@naver.com>
2017-09-04 16:49:18 +0900

    일부 수정 : 이미지 명, partner.html 스타일 css 파일 추가와 css파일 루트 변경

--- js/common.js
+++ js/common.js
@@ -1,18 +1,18 @@
-// �꾩뿭 蹂���
+// 전역 변수
 var errmsg = "";
 var errfld = null;
 
-// �꾨뱶 寃���
+// 필드 검사
 function check_field(fld, msg)
 {
     if ((fld.value = trim(fld.value)) == "")
         error_field(fld, msg);
-    else
+    elsex
         clear_field(fld);
     return;
 }
 
-// �꾨뱶 �ㅻ쪟 �쒖떆
+// 필드 오류 표시
 function error_field(fld, msg)
 {
     if (msg != "")
@@ -21,7 +21,7 @@ function error_field(fld, msg)
     fld.style.background = "#BDDEF7";
 }
 
-// �꾨뱶瑜� 源⑤걮�섍쾶
+// 필드를 깨끗하게
 function clear_field(fld)
 {
     fld.style.background = "#FFFFFF";
@@ -59,8 +59,8 @@ function trim(s)
     return t;
 }
 
-// �먮컮�ㅽ겕由쏀듃濡� PHP�� number_format �됰궡瑜� ��
-// �レ옄�� , 瑜� 異쒕젰
+// 자바스크립트로 PHP의 number_format 흉내를 냄
+// 숫자에 , 를 출력
 function number_format(data)
 {
 
@@ -99,21 +99,21 @@ function number_format(data)
     return number;
 }
 
-// �� 李�
+// 새 창
 function popup_window(url, winname, opt)
 {
     window.open(url, winname, opt);
 }
 
 
-// �쇰찓�� 李�
+// 폼메일 창
 function popup_formmail(url)
 {
     opt = 'scrollbars=yes,width=417,height=385,top=10,left=20';
     popup_window(url, "wformmail", opt);
 }
 
-// , 瑜� �놁븻��.
+// , 를 없앤다.
 function no_comma(data)
 {
     var tmp = '';
@@ -128,10 +128,10 @@ function no_comma(data)
     return tmp;
 }
 
-// ��젣 寃��� �뺤씤
+// 삭제 검사 확인
 function del(href)
 {
-    if(confirm("�쒕쾲 ��젣�� �먮즺�� 蹂듦뎄�� 諛⑸쾿�� �놁뒿�덈떎.\n\n�뺣쭚 ��젣�섏떆寃좎뒿�덇퉴?")) {
+    if(confirm("한번 삭제한 자료는 복구할 방법이 없습니다.\n\n정말 삭제하시겠습니까?")) {
         var iev = -1;
         if (navigator.appName == 'Microsoft Internet Explorer') {
             var ua = navigator.userAgent;
@@ -140,7 +140,7 @@ function del(href)
                 iev = parseFloat(RegExp.$1);
         }
 
-        // IE6 �댄븯�먯꽌 �쒓�源⑥쭚 諛⑹�
+        // IE6 이하에서 한글깨짐 방지
         if (iev != -1 && iev < 7) {
             document.location.href = encodeURI(href);
         } else {
@@ -149,7 +149,7 @@ function del(href)
     }
 }
 
-// 荑좏궎 �낅젰
+// 쿠키 입력
 function set_cookie(name, value, expirehours, domain)
 {
     var today = new Date();
@@ -160,7 +160,7 @@ function set_cookie(name, value, expirehours, domain)
     }
 }
 
-// 荑좏궎 �살쓬
+// 쿠키 얻음
 function get_cookie(name)
 {
     var find_sw = false;
@@ -192,7 +192,7 @@ function get_cookie(name)
     return "";
 }
 
-// 荑좏궎 吏���
+// 쿠키 지움
 function delete_cookie(name)
 {
     var today = new Date();
@@ -236,7 +236,7 @@ function textarea_increase(id, row)
     document.getElementById(id).rows += row;
 }
 
-// 湲��レ옄 寃���
+// 글숫자 검사
 function check_byte(content, target)
 {
     var i = 0;
@@ -252,13 +252,13 @@ function check_byte(content, target)
             cnt += 1;
         }
     }
-    // �レ옄瑜� 異쒕젰
+    // 숫자를 출력
     document.getElementById(target).innerHTML = cnt;
 
     return cnt;
 }
 
-// 釉뚮씪�곗��먯꽌 �ㅻ툕�앺듃�� �쇱そ 醫뚰몴
+// 브라우저에서 오브젝트의 왼쪽 좌표
 function get_left_pos(obj)
 {
     var parentObj = null;
@@ -275,7 +275,7 @@ function get_left_pos(obj)
     return left;
 }
 
-// 釉뚮씪�곗��먯꽌 �ㅻ툕�앺듃�� �곷떒 醫뚰몴
+// 브라우저에서 오브젝트의 상단 좌표
 function get_top_pos(obj)
 {
     var parentObj = null;
@@ -326,7 +326,7 @@ $(document).ready(function(){
 });
 
 /**
- * �ъ씤�� 李�
+ * 포인트 창
  **/
 var win_point = function(href) {
     var new_win = window.open(href, 'win_point', 'left=100,top=100,width=600, height=600, scrollbars=1');
@@ -334,7 +334,7 @@ var win_point = function(href) {
 }
 
 /**
- * 履쎌� 李�
+ * 쪽지 창
  **/
 var win_memo = function(href) {
     var new_win = window.open(href, 'win_memo', 'left=100,top=100,width=620,height=500,scrollbars=1');
@@ -342,7 +342,7 @@ var win_memo = function(href) {
 }
 
 /**
- * 硫붿씪 李�
+ * 메일 창
  **/
 var win_email = function(href) {
     var new_win = window.open(href, 'win_email', 'left=100,top=100,width=600,height=580,scrollbars=0');
@@ -350,7 +350,7 @@ var win_email = function(href) {
 }
 
 /**
- * �먭린�뚭컻 李�
+ * 자기소개 창
  **/
 var win_profile = function(href) {
     var new_win = window.open(href, 'win_profile', 'left=100,top=100,width=620,height=510,scrollbars=1');
@@ -358,7 +358,7 @@ var win_profile = function(href) {
 }
 
 /**
- * �ㅽ겕�� 李�
+ * 스크랩 창
  **/
 var win_scrap = function(href) {
     var new_win = window.open(href, 'win_scrap', 'left=100,top=100,width=600,height=600,scrollbars=1');
@@ -366,7 +366,7 @@ var win_scrap = function(href) {
 }
 
 /**
- * �덊럹�댁� 李�
+ * 홈페이지 창
  **/
 var win_homepage = function(href) {
     var new_win = window.open(href, 'win_homepage', '');
@@ -374,47 +374,47 @@ var win_homepage = function(href) {
 }
 
 /**
- * �고렪踰덊샇 李�
+ * 우편번호 창
  **/
 var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_jibeon) {
     if(typeof daum === 'undefined'){
-        alert("�ㅼ쓬 �고렪踰덊샇 postcode.v2.js �뚯씪�� 濡쒕뱶�섏� �딆븯�듬땲��.");
+        alert("다음 우편번호 postcode.v2.js 파일이 로드되지 않았습니다.");
         return false;
     }
 
-    var zip_case = 1;   //0�대㈃ �덉씠��, 1�대㈃ �섏씠吏��� �쇱썙 �ｊ린, 2�대㈃ �덉갹
+    var zip_case = 1;   //0이면 레이어, 1이면 페이지에 끼워 넣기, 2이면 새창
 
     var complete_fn = function(data){
-        // �앹뾽�먯꽌 寃��됯껐怨� ��ぉ�� �대┃�덉쓣�� �ㅽ뻾�� 肄붾뱶瑜� �묒꽦�섎뒗 遺�遺�.
+        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-        // 媛� 二쇱냼�� �몄텧 洹쒖튃�� �곕씪 二쇱냼瑜� 議고빀�쒕떎.
-        // �대젮�ㅻ뒗 蹂��섍� 媛믪씠 �녿뒗 寃쎌슦�� 怨듬갚('')媛믪쓣 媛�吏�誘�濡�, �대� 李멸퀬�섏뿬 遺꾧린 �쒕떎.
-        var fullAddr = ''; // 理쒖쥌 二쇱냼 蹂���
-        var extraAddr = ''; // 議고빀�� 二쇱냼 蹂���
+        // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+        // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+        var fullAddr = ''; // 최종 주소 변수
+        var extraAddr = ''; // 조합형 주소 변수
 
-        // �ъ슜�먭� �좏깮�� 二쇱냼 ���낆뿉 �곕씪 �대떦 二쇱냼 媛믪쓣 媛��몄삩��.
-        if (data.userSelectedType === 'R') { // �ъ슜�먭� �꾨줈紐� 二쇱냼瑜� �좏깮�덉쓣 寃쎌슦
+        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+        if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
             fullAddr = data.roadAddress;
 
-        } else { // �ъ슜�먭� 吏�踰� 二쇱냼瑜� �좏깮�덉쓣 寃쎌슦(J)
+        } else { // 사용자가 지번 주소를 선택했을 경우(J)
             fullAddr = data.jibunAddress;
         }
 
-        // �ъ슜�먭� �좏깮�� 二쇱냼媛� �꾨줈紐� ���낆씪�� 議고빀�쒕떎.
+        // 사용자가 선택한 주소가 도로명 타입일때 조합한다.
         if(data.userSelectedType === 'R'){
-            //踰뺤젙�숇챸�� �덉쓣 寃쎌슦 異붽��쒕떎.
+            //법정동명이 있을 경우 추가한다.
             if(data.bname !== ''){
                 extraAddr += data.bname;
             }
-            // 嫄대Ъ紐낆씠 �덉쓣 寃쎌슦 異붽��쒕떎.
+            // 건물명이 있을 경우 추가한다.
             if(data.buildingName !== ''){
                 extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
             }
-            // 議고빀�뺤＜�뚯쓽 �좊Т�� �곕씪 �묒そ�� 愿꾪샇瑜� 異붽��섏뿬 理쒖쥌 二쇱냼瑜� 留뚮뱺��.
+            // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
             extraAddr = (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
         }
 
-        // �고렪踰덊샇�� 二쇱냼 �뺣낫瑜� �대떦 �꾨뱶�� �ｊ퀬, 而ㅼ꽌瑜� �곸꽭二쇱냼 �꾨뱶濡� �대룞�쒕떎.
+        // 우편번호와 주소 정보를 해당 필드에 넣고, 커서를 상세주소 필드로 이동한다.
         var of = document[frm_name];
 
         of[frm_zip].value = data.zonecode;
@@ -430,7 +430,7 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_j
     };
 
     switch(zip_case) {
-        case 1 :    //iframe�� �댁슜�섏뿬 �섏씠吏��� �쇱썙 �ｊ린
+        case 1 :    //iframe을 이용하여 페이지에 끼워 넣기
             var daum_pape_id = 'daum_juso_page'+frm_zip,
                 element_wrap = document.getElementById(daum_pape_id),
                 currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
@@ -438,7 +438,7 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_j
                 element_wrap = document.createElement("div");
                 element_wrap.setAttribute("id", daum_pape_id);
                 element_wrap.style.cssText = 'display:none;border:1px solid;left:0;width:100%;height:300px;margin:5px 0;position:relative;-webkit-overflow-scrolling:touch;';
-                element_wrap.innerHTML = '<img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnFoldWrap" style="cursor:pointer;position:absolute;right:0px;top:-21px;z-index:1" class="close_daum_juso" alt="�묎린 踰꾪듉">';
+                element_wrap.innerHTML = '<img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnFoldWrap" style="cursor:pointer;position:absolute;right:0px;top:-21px;z-index:1" class="close_daum_juso" alt="접기 버튼">';
                 jQuery('form[name="'+frm_name+'"]').find('input[name="'+frm_addr1+'"]').before(element_wrap);
                 jQuery("#"+daum_pape_id).off("click", ".close_daum_juso").on("click", ".close_daum_juso", function(e){
                     e.preventDefault();
@@ -449,13 +449,13 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_j
             new daum.Postcode({
                 oncomplete: function(data) {
                     complete_fn(data);
-                    // iframe�� �ｌ� element瑜� �덈낫�닿쾶 �쒕떎.
+                    // iframe을 넣은 element를 안보이게 한다.
                     element_wrap.style.display = 'none';
-                    // �고렪踰덊샇 李얘린 �붾㈃�� 蹂댁씠湲� �댁쟾�쇰줈 scroll �꾩튂瑜� �섎룎由곕떎.
+                    // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
                     document.body.scrollTop = currentScroll;
                 },
-                // �고렪踰덊샇 李얘린 �붾㈃ �ш린媛� 議곗젙�섏뿀�꾨븣 �ㅽ뻾�� 肄붾뱶瑜� �묒꽦�섎뒗 遺�遺�.
-                // iframe�� �ｌ� element�� �믪씠媛믪쓣 議곗젙�쒕떎.
+                // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분.
+                // iframe을 넣은 element의 높이값을 조정한다.
                 onresize : function(size) {
                     element_wrap.style.height = size.height + "px";
                 },
@@ -463,24 +463,24 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_j
                 height : '100%'
             }).embed(element_wrap);
 
-            // iframe�� �ｌ� element瑜� 蹂댁씠寃� �쒕떎.
+            // iframe을 넣은 element를 보이게 한다.
             element_wrap.style.display = 'block';
             break;
-        case 2 :    //�덉갹�쇰줈 �꾩슦湲�
+        case 2 :    //새창으로 띄우기
             new daum.Postcode({
                 oncomplete: function(data) {
                     complete_fn(data);
                 }
             }).open();
             break;
-        default :   //iframe�� �댁슜�섏뿬 �덉씠�� �꾩슦湲�
+        default :   //iframe을 이용하여 레이어 띄우기
             var rayer_id = 'daum_juso_rayer'+frm_zip,
                 element_layer = document.getElementById(rayer_id);
             if (element_layer == null) {
                 element_layer = document.createElement("div");
                 element_layer.setAttribute("id", rayer_id);
                 element_layer.style.cssText = 'display:none;border:5px solid;position:fixed;width:300px;height:460px;left:50%;margin-left:-155px;top:50%;margin-top:-235px;overflow:hidden;-webkit-overflow-scrolling:touch;z-index:10000';
-                element_layer.innerHTML = '<img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnCloseLayer" style="cursor:pointer;position:absolute;right:-3px;top:-3px;z-index:1" class="close_daum_juso" alt="�リ린 踰꾪듉">';
+                element_layer.innerHTML = '<img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnCloseLayer" style="cursor:pointer;position:absolute;right:-3px;top:-3px;z-index:1" class="close_daum_juso" alt="닫기 버튼">';
                 document.body.appendChild(element_layer);
                 jQuery("#"+rayer_id).off("click", ".close_daum_juso").on("click", ".close_daum_juso", function(e){
                     e.preventDefault();
@@ -491,20 +491,20 @@ var win_zip = function(frm_name, frm_zip, frm_addr1, frm_addr2, frm_addr3, frm_j
             new daum.Postcode({
                 oncomplete: function(data) {
                     complete_fn(data);
-                    // iframe�� �ｌ� element瑜� �덈낫�닿쾶 �쒕떎.
+                    // iframe을 넣은 element를 안보이게 한다.
                     element_layer.style.display = 'none';
                 },
                 width : '100%',
                 height : '100%'
             }).embed(element_layer);
 
-            // iframe�� �ｌ� element瑜� 蹂댁씠寃� �쒕떎.
+            // iframe을 넣은 element를 보이게 한다.
             element_layer.style.display = 'block';
     }
 }
 
 /**
- * �덈줈�� 鍮꾨�踰덊샇 遺꾩떎 李� : 101123
+ * 새로운 비밀번호 분실 창 : 101123
  **/
 win_password_lost = function(href)
 {
@@ -513,7 +513,7 @@ win_password_lost = function(href)
 }
 
 /**
- * �ㅻЦ議곗궗 寃곌낵
+ * 설문조사 결과
  **/
 var win_poll = function(href) {
     var new_win = window.open(href, 'win_poll', 'width=616, height=500, scrollbars=1');
@@ -521,8 +521,8 @@ var win_poll = function(href) {
 }
 
 /**
- * �ㅽ겕由곕━�� 誘몄궗�⑹옄瑜� �꾪븳 �ㅽ겕由쏀듃 - 吏��댁븘鍮� 2013-04-22
- * alt 媛믩쭔 媛뽯뒗 洹몃옒�� 留곹겕�� 留덉슦�ㅼ삤踰� �� title 媛� 遺���, 留덉슦�ㅼ븘�� �� title 媛� �쒓굅
+ * 스크린리더 미사용자를 위한 스크립트 - 지운아빠 2013-04-22
+ * alt 값만 갖는 그래픽 링크에 마우스오버 시 title 값 부여, 마우스아웃 시 title 값 제거
  **/
 $(function() {
     $('a img').mouseover(function() {
@@ -534,7 +534,7 @@ $(function() {
 });
 
 /**
- * �띿뒪�� 由ъ궗�댁쫰
+ * 텍스트 리사이즈
 **/
 function font_resize(id, rmv_class, add_class)
 {
@@ -589,7 +589,7 @@ $(function(){
     });
     */
 
-    // �ъ씠�쒕럭
+    // 사이드뷰
     var sv_hide = false;
     $(".sv_member, .sv_guest").click(function() {
         $(".sv").removeClass("sv_on");
@@ -619,7 +619,7 @@ $(function(){
         sv_hide = true;
     });
 
-    // ���됲듃 ul
+    // 셀렉트 ul
     var sel_hide = false;
     $('.sel_btn').click(function() {
         $('.sel_ul').removeClass('sel_on');
@@ -644,19 +644,19 @@ $(function(){
     });
 
     $(document).click(function() {
-        if(sv_hide) { // �ъ씠�쒕럭 �댁젣
+        if(sv_hide) { // 사이드뷰 해제
             $(".sv").removeClass("sv_on");
         }
-        if (sel_hide) { // ���됲듃 ul �댁젣
+        if (sel_hide) { // 셀렉트 ul 해제
             $('.sel_ul').removeClass('sel_on');
         }
     });
 
     $(document).focusin(function() {
-        if(sv_hide) { // �ъ씠�쒕럭 �댁젣
+        if(sv_hide) { // 사이드뷰 해제
             $(".sv").removeClass("sv_on");
         }
-        if (sel_hide) { // ���됲듃 ul �댁젣
+        if (sel_hide) { // 셀렉트 ul 해제
             $('.sel_ul').removeClass('sel_on');
         }
     });
--- js/wrest.js
+++ js/wrest.js
@@ -3,7 +3,7 @@ var wrestFld = null;
 var wrestFldDefaultColor = "";
 //var wrestFldBackColor = "#ff3061";
 
-// subject �띿꽦媛믪쓣 �살뼱 return, �놁쑝硫� tag�� name�� �섍�
+// subject 속성값을 얻어 return, 없으면 tag의 name을 넘김
 function wrestItemname(fld)
 {
     //return fld.getAttribute("title") ? fld.getAttribute("title") : ( fld.getAttribute("alt") ? fld.getAttribute("alt") : fld.name );
@@ -31,26 +31,26 @@ function wrestItemname(fld)
     }
 }
 
-// �묒そ 怨듬갚 �놁븷湲�
+// 양쪽 공백 없애기
 function wrestTrim(fld)
 {
-    var pattern = /(^\s+)|(\s+$)/g; // \s 怨듬갚 臾몄옄
+    var pattern = /(^\s+)|(\s+$)/g; // \s 공백 문자
     return fld.value.replace(pattern, "");
 }
 
-// �꾩닔 �낅젰 寃���
+// 필수 입력 검사
 function wrestRequired(fld)
 {
     if (wrestTrim(fld) == "") {
         if (wrestFld == null) {
-            // ���됲듃諛뺤뒪�� 寃쎌슦�먮룄 �꾩닔 �좏깮 寃��ы빀�덈떎.
-            wrestMsg = wrestItemname(fld) + " : �꾩닔 "+(fld.type=="select-one"?"�좏깮":"�낅젰")+"�낅땲��.\n";
+            // 셀렉트박스일 경우에도 필수 선택 검사합니다.
+            wrestMsg = wrestItemname(fld) + " : 필수 "+(fld.type=="select-one"?"선택":"입력")+"입니다.\n";
             wrestFld = fld;
         }
     }
 }
 
-// 源��좎슜 2006.3 - �꾪솕踰덊샇(�대���) �뺤떇 寃��� : 123-123(4)-5678
+// 김선용 2006.3 - 전화번호(휴대폰) 형식 검사 : 123-123(4)-5678
 function wrestTelNum(fld)
 {
     if (!wrestTrim(fld)) return;
@@ -58,106 +58,106 @@ function wrestTelNum(fld)
     var pattern = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/;
     if(!pattern.test(fld.value)){
         if(wrestFld == null){
-            wrestMsg = wrestItemname(fld)+" : �꾪솕踰덊샇 �뺤떇�� �щ컮瑜댁� �딆뒿�덈떎.\n\n�섏씠��(-)�� �ы븿�섏뿬 �낅젰�섏꽭��.\n";
+            wrestMsg = wrestItemname(fld)+" : 전화번호 형식이 올바르지 않습니다.\n\n하이픈(-)을 포함하여 입력하세요.\n";
             wrestFld = fld;
             fld.select();
         }
     }
 }
 
-// �대찓�쇱＜�� �뺤떇 寃���
+// 이메일주소 형식 검사
 function wrestEmail(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    //var pattern = /(\S+)@(\S+)\.(\S+)/; �대찓�쇱＜�뚯뿉 �쒓� �ъ슜��
+    //var pattern = /(\S+)@(\S+)\.(\S+)/; 이메일주소에 한글 사용시
     var pattern = /([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)\.([0-9a-zA-Z_-]+)/;
     if (!pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + " : �대찓�쇱＜�� �뺤떇�� �꾨떃�덈떎.\n";
+            wrestMsg = wrestItemname(fld) + " : 이메일주소 형식이 아닙니다.\n";
             wrestFld = fld;
         }
     }
 }
 
-// �쒓��몄� 寃��� (�먯쓬, 紐⑥쓬 議고빀�� �쒓�留� 媛���)
+// 한글인지 검사 (자음, 모음 조합된 한글만 가능)
 function wrestHangul(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    //var pattern = /([^媛�-��\x20])/i;
-    var pattern = /([^媛�-��\x20])/;
+    //var pattern = /([^가-힣\x20])/i;
+    var pattern = /([^가-힣\x20])/;
 
     if (pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + ' : �쒓��� �꾨떃�덈떎. (�먯쓬, 紐⑥쓬 議고빀�� �쒓�留� 媛���)\n';
+            wrestMsg = wrestItemname(fld) + ' : 한글이 아닙니다. (자음, 모음 조합된 한글만 가능)\n';
             wrestFld = fld;
         }
     }
 }
 
-// �쒓��몄� 寃���2 (�먯쓬, 紐⑥쓬留� �덈뒗 �쒓��� 媛���)
+// 한글인지 검사2 (자음, 모음만 있는 한글도 가능)
 function wrestHangul2(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    var pattern = /([^媛�-�ｃ꽦-�롢뀖-��\x20])/i;
-    //var pattern = /([^媛�-�ｃ꽦-�롢뀖-��\x20])/;
+    var pattern = /([^가-힣ㄱ-ㅎㅏ-ㅣ\x20])/i;
+    //var pattern = /([^가-힣ㄱ-ㅎㅏ-ㅣ\x20])/;
 
     if (pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + ' : �쒓��� �꾨떃�덈떎.\n';
+            wrestMsg = wrestItemname(fld) + ' : 한글이 아닙니다.\n';
             wrestFld = fld;
         }
     }
 }
 
-// �쒓�,�곷Ц,�レ옄�몄� 寃���3
+// 한글,영문,숫자인지 검사3
 function wrestHangulAlNum(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    var pattern = /([^媛�-��\x20^a-z^A-Z^0-9])/i;
+    var pattern = /([^가-힣\x20^a-z^A-Z^0-9])/i;
 
     if (pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + ' : �쒓�, �곷Ц, �レ옄媛� �꾨떃�덈떎.\n';
+            wrestMsg = wrestItemname(fld) + ' : 한글, 영문, 숫자가 아닙니다.\n';
             wrestFld = fld;
         }
     }
 }
 
-// �쒓�,�곷Ц �몄� 寃���
+// 한글,영문 인지 검사
 function wrestHangulAlpha(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    var pattern = /([^媛�-��\x20^a-z^A-Z])/i;
+    var pattern = /([^가-힣\x20^a-z^A-Z])/i;
 
     if (pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + ' : �쒓�, �곷Ц�� �꾨떃�덈떎.\n';
+            wrestMsg = wrestItemname(fld) + ' : 한글, 영문이 아닙니다.\n';
             wrestFld = fld;
         }
     }
 }
 
-// �レ옄�몄�寃���
-// 諛곕�瑜멸�轅��대떂 異붽� (http://dasir.com) 2003-06-24
+// 숫자인지검사
+// 배부른꿀꿀이님 추가 (http://dasir.com) 2003-06-24
 function wrestNumeric(fld)
 {
     if (fld.value.length > 0) {
         for (i = 0; i < fld.value.length; i++) {
             if (fld.value.charAt(i) < '0' || fld.value.charAt(i) > '9') {
-                wrestMsg = wrestItemname(fld) + " : �レ옄媛� �꾨떃�덈떎.\n";
+                wrestMsg = wrestItemname(fld) + " : 숫자가 아닙니다.\n";
                 wrestFld = fld;
             }
         }
     }
 }
 
-// �곷Ц�� 寃���
-// 諛곕�瑜멸�轅��대떂 異붽� (http://dasir.com) 2003-06-24
+// 영문자 검사
+// 배부른꿀꿀이님 추가 (http://dasir.com) 2003-06-24
 function wrestAlpha(fld)
 {
     if (!wrestTrim(fld)) return;
@@ -166,14 +166,14 @@ function wrestAlpha(fld)
 
     if (!pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + " : �곷Ц�� �꾨떃�덈떎.\n";
+            wrestMsg = wrestItemname(fld) + " : 영문이 아닙니다.\n";
             wrestFld = fld;
         }
     }
 }
 
-// �곷Ц�먯� �レ옄 寃���
-// 諛곕�瑜멸�轅��대떂 異붽� (http://dasir.com) 2003-07-07
+// 영문자와 숫자 검사
+// 배부른꿀꿀이님 추가 (http://dasir.com) 2003-07-07
 function wrestAlNum(fld)
 {
    if (!wrestTrim(fld)) return;
@@ -182,13 +182,13 @@ function wrestAlNum(fld)
 
    if (!pattern.test(fld.value)) {
        if (wrestFld == null) {
-           wrestMsg = wrestItemname(fld) + " : �곷Ц �먮뒗 �レ옄媛� �꾨떃�덈떎.\n";
+           wrestMsg = wrestItemname(fld) + " : 영문 또는 숫자가 아닙니다.\n";
            wrestFld = fld;
        }
    }
 }
 
-// �곷Ц�먯� �レ옄 洹몃━怨� _ 寃���
+// 영문자와 숫자 그리고 _ 검사
 function wrestAlNum_(fld)
 {
    if (!wrestTrim(fld)) return;
@@ -197,13 +197,13 @@ function wrestAlNum_(fld)
 
    if (!pattern.test(fld.value)) {
        if (wrestFld == null) {
-           wrestMsg = wrestItemname(fld) + " : �곷Ц, �レ옄, _ 媛� �꾨떃�덈떎.\n";
+           wrestMsg = wrestItemname(fld) + " : 영문, 숫자, _ 가 아닙니다.\n";
            wrestFld = fld;
        }
    }
 }
 
-// 理쒖냼 湲몄씠 寃���
+// 최소 길이 검사
 function wrestMinLength(fld)
 {
     if (!wrestTrim(fld)) return;
@@ -212,28 +212,28 @@ function wrestMinLength(fld)
 
     if (wrestFld == null) {
         if (fld.value.length < parseInt(minlength)) {
-            wrestMsg = wrestItemname(fld) + " : 理쒖냼 "+minlength+"湲��� �댁긽 �낅젰�섏꽭��.\n";
+            wrestMsg = wrestItemname(fld) + " : 최소 "+minlength+"글자 이상 입력하세요.\n";
             wrestFld = fld;
         }
     }
 }
 
-// �대�吏� �뺤옣��
+// 이미지 확장자
 function wrestImgExt(fld)
 {
     if (!wrestTrim(fld)) return;
 
-    var pattern = /\.(gif|jpg|png)$/i; // jpeg �� �쒖쇅
+    var pattern = /\.(gif|jpg|png)$/i; // jpeg 는 제외
     if(!pattern.test(fld.value)){
         if(wrestFld == null){
-            wrestMsg = wrestItemname(fld)+" : �대�吏� �뚯씪�� �꾨떃�덈떎.\n.gif .jpg .png �뚯씪留� 媛��ν빀�덈떎.\n";
+            wrestMsg = wrestItemname(fld)+" : 이미지 파일이 아닙니다.\n.gif .jpg .png 파일만 가능합니다.\n";
             wrestFld = fld;
             fld.select();
         }
     }
 }
 
-// �뺤옣��
+// 확장자
 function wrestExtension(fld, css)
 {
     if (!wrestTrim(fld)) return;
@@ -244,26 +244,26 @@ function wrestExtension(fld, css)
 
     if (wrestFld == null) {
         if (ext.toLowerCase() < str[1].toLowerCase()) {
-            wrestMsg = wrestItemname(fld) + " : ."+str[1]+" �뚯씪留� 媛��ν빀�덈떎.\n";
+            wrestMsg = wrestItemname(fld) + " : ."+str[1]+" 파일만 가능합니다.\n";
             wrestFld = fld;
         }
     }
 }
 
-// 怨듬갚 寃��ы썑 怨듬갚�� "" 濡� 蹂���
+// 공백 검사후 공백을 "" 로 변환
 function wrestNospace(fld)
 {
-    var pattern = /(\s)/g; // \s 怨듬갚 臾몄옄
+    var pattern = /(\s)/g; // \s 공백 문자
 
     if (pattern.test(fld.value)) {
         if (wrestFld == null) {
-            wrestMsg = wrestItemname(fld) + " : 怨듬갚�� �놁뼱�� �⑸땲��.\n";
+            wrestMsg = wrestItemname(fld) + " : 공백이 없어야 합니다.\n";
             wrestFld = fld;
         }
     }
 }
 
-// submit �� �� �띿꽦�� 寃��ы븳��.
+// submit 할 때 속성을 검사한다.
 function wrestSubmit()
 {
     wrestMsg = "";
@@ -271,12 +271,12 @@ function wrestSubmit()
 
     var attr = null;
 
-    // �대떦�쇱뿉 ���� �붿냼�� 媛쒖닔留뚰겮 �뚮젮��
+    // 해당폼에 대한 요소의 개수만큼 돌려라
     for (var i=0; i<this.elements.length; i++) {
         var el = this.elements[i];
 
-        // Input tag �� type �� text, file, password �쇰븣留�
-        // ���됲듃 諛뺤뒪�쇰븣�� �꾩닔 �좏깮 寃��ы빀�덈떎. select-one
+        // Input tag 의 type 이 text, file, password 일때만
+        // 셀렉트 박스일때도 필수 선택 검사합니다. select-one
         if (el.type=="text" || el.type=="hidden" || el.type=="file" || el.type=="password" || el.type=="select-one" || el.type=="textarea") {
             if (el.getAttribute("required") != null) {
                 wrestRequired(el);
@@ -286,11 +286,11 @@ function wrestSubmit()
                 wrestMinLength(el);
             }
 
-            var array_css = el.className.split(" "); // class 瑜� 怨듬갚�쇰줈 �섎닎
+            var array_css = el.className.split(" "); // class 를 공백으로 나눔
 
             el.style.backgroundColor = wrestFldDefaultColor;
 
-            // 諛곗뿴�� 湲몄씠留뚰겮 �뚮젮��
+            // 배열의 길이만큼 돌려라
             for (var k=0; k<array_css.length; k++) {
                 var css = array_css[k];
                 switch (css) {
@@ -306,7 +306,7 @@ function wrestSubmit()
                     case "alpha"        : wrestAlpha(el); break;
                     case "alnum"        : wrestAlNum(el); break;
                     case "alnum_"       : wrestAlNum_(el); break;
-                    case "telnum"       : wrestTelNum(el); break; // 源��좎슜 2006.3 - �꾪솕踰덊샇 �뺤떇 寃���
+                    case "telnum"       : wrestTelNum(el); break; // 김선용 2006.3 - 전화번호 형식 검사
                     case "imgext"       : wrestImgExt(el); break;
                     default :
                         if (/^extension\=/.test(css)) {
@@ -317,16 +317,16 @@ function wrestSubmit()
         } // if (el)
     } // for (i)
 
-    // �꾨뱶媛� null �� �꾨땲�쇰㈃ �ㅻ쪟硫붿꽭吏� 異쒕젰�� �ъ빱�ㅻ� �대떦 �ㅻ쪟 �꾨뱶濡� ���
-    // �ㅻ쪟 �꾨뱶�� 諛곌꼍�됱긽�� 諛붽씔��.
+    // 필드가 null 이 아니라면 오류메세지 출력후 포커스를 해당 오류 필드로 옮김
+    // 오류 필드는 배경색상을 바꾼다.
     if (wrestFld != null) {
-        // 寃쎄퀬硫붿꽭吏� 異쒕젰
+        // 경고메세지 출력
         alert(wrestMsg);
 
         if (wrestFld.style.display != "none") {
             var id = wrestFld.getAttribute("id");
 
-            // �ㅻ쪟硫붿꽭吏�瑜� �꾪븳 element 異붽�
+            // 오류메세지를 위한 element 추가
             var msg_el = document.createElement("strong");
             msg_el.id = "msg_"+id;
             msg_el.className = "msg_sound_only";
@@ -352,11 +352,11 @@ function wrestSubmit()
 }
 
 
-// 珥덇린�� onsubmit�� 媛�濡쒖콈�꾨줉 �쒕떎.
+// 초기에 onsubmit을 가로채도록 한다.
 function wrestInitialized()
 {
     for (var i = 0; i < document.forms.length; i++) {
-        // onsubmit �대깽�멸� �덈떎硫� ���ν빐 �볥뒗��.
+        // onsubmit 이벤트가 있다면 저장해 놓는다.
         if (document.forms[i].onsubmit) {
             document.forms[i].oldsubmit = document.forms[i].onsubmit;
         }
@@ -364,7 +364,7 @@ function wrestInitialized()
     }
 }
 
-// �쇳븘�� �먮룞寃���
+// 폼필드 자동검사
 $(document).ready(function(){
     // onload
     wrestInitialized();
